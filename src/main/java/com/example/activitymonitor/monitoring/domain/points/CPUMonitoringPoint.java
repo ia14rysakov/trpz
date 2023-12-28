edaea933e09880aae709edc4de2853849538c89f
ajax-rysakov-b <rysakov.b@ajax.systems>
2023-12-29 00:44:24 +0200

    fix ui

--- src/main/java/com/example/activitymonitor/monitoring/domain/points/CPUMonitoringPoint.java
+++ src/main/java/com/example/activitymonitor/monitoring/domain/points/CPUMonitoringPoint.java
@@ -2,6 +2,7 @@ package com.example.activitymonitor.monitoring.domain.points;
 
 import com.example.activitymonitor.monitoring.domain.MonitoringPoint;
 import lombok.Getter;
+import lombok.ToString;
 
 import java.lang.management.ManagementFactory;
 import java.lang.management.OperatingSystemMXBean;
@@ -9,6 +10,7 @@ import java.time.LocalDateTime;
 import java.util.stream.Stream;
 
 @Getter
+@ToString
 public class CPUMonitoringPoint implements MonitoringPoint {
     private double cpuUsage;
     private LocalDateTime timestamp = LocalDateTime.now();
--- src/main/java/com/example/activitymonitor/monitoring/infrastructure/rest/dto/MonitoringRequestDto.java
+++ src/main/java/com/example/activitymonitor/monitoring/infrastructure/rest/dto/MonitoringRequestDto.java
@@ -2,9 +2,11 @@ package com.example.activitymonitor.monitoring.infrastructure.rest.dto;
 
 import lombok.Getter;
 import lombok.Setter;
+import lombok.ToString;
 
 @Getter
 @Setter
+@ToString
 public class MonitoringRequestDto {
 
     private String monitoringType;
