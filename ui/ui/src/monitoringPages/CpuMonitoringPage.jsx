ac10a133dfc416357144b9200e2551b728a72b64
ajax-rysakov-b <rysakov.b@ajax.systems>
2023-12-29 00:53:33 +0200

    fix ui

--- ui/ui/src/monitoringPages/CpuMonitoringPage.jsx
+++ ui/ui/src/monitoringPages/CpuMonitoringPage.jsx
@@ -1,5 +1,4 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
 import { Line } from 'react-chartjs-2';
 import 'chart.js/auto';
 
@@ -17,31 +16,30 @@ const CpuMonitoringPage = () => {
     });
 
     useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await axios.post('http://localhost:8080/monitoring/start', {
-                    monitoringType: "cpuLoad",
-                    osType: "Windows"
-                });
-                response.data.forEach(point => {
-                    console.log('Incoming point:', point); // Log each incoming point
+        const eventSource = new EventSource('http://localhost:8080/monitoring/start');
+        eventSource.onmessage = (event) => {
+            const point = JSON.parse(event.data);
 
-                    // Append to chart data
-                    setChartData(prevData => ({
-                        labels: [...prevData.labels, new Date(point.timestamp).toLocaleTimeString()],
-                        datasets: [{
-                            ...prevData.datasets[0],
-                            data: [...prevData.datasets[0].data, point.cpuUsage]
-                        }]
-                    }));
-                });
+            console.log('Incoming point:', point); // Log each incoming point
 
-            } catch (error) {
-                console.error('Error sending monitoring request:', error);
-            }
+            // Append to chart data
+            setChartData(prevData => ({
+                labels: [...prevData.labels, new Date(point.timestamp).toLocaleTimeString()],
+                datasets: [{
+                    ...prevData.datasets[0],
+                    data: [...prevData.datasets[0].data, point.cpuUsage]
+                }]
+            }));
         };
 
-        fetchData();
+        eventSource.onerror = (error) => {
+            console.error('EventSource failed:', error);
+            eventSource.close();
+        };
+
+        return () => {
+            eventSource.close();
+        };
     }, []); // Empty dependency array ensures this effect runs once on mount
 
     return (
