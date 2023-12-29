dfeff9381f07f19f5946eb69eec62eb50ee17bda
ajax-rysakov-b <rysakov.b@ajax.systems>
2023-12-29 11:25:01 +0200

    add memory page

--- src/main/java/com/example/activitymonitor/monitoring/domain/points/MemoryMonitoringPoint.java
+++ src/main/java/com/example/activitymonitor/monitoring/domain/points/MemoryMonitoringPoint.java
@@ -3,9 +3,13 @@ package com.example.activitymonitor.monitoring.domain.points;
 import com.example.activitymonitor.monitoring.domain.MonitoringPoint;
 import lombok.Getter;
 
+import java.time.LocalDateTime;
+
 @Getter
 public class MemoryMonitoringPoint implements MonitoringPoint {
     private double memoryUsage;
+
+    private LocalDateTime timestamp = LocalDateTime.now();
 
     public MemoryMonitoringPoint(double memoryUsage) {
         this.memoryUsage = memoryUsage;
--- ui/ui/src/monitoringPages/MemoryMonitoringPage.jsx
+++ ui/ui/src/monitoringPages/MemoryMonitoringPage.jsx
@@ -1,48 +1,35 @@
 import React, { useState, useEffect } from 'react';
-import axios from 'axios';
-import { useLocation } from 'react-router-dom';
-import {
-    Chart as ChartJS,
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    Title,
-    Tooltip,
-    Legend,
-    TimeScale,
-} from 'chart.js';
-import 'chartjs-adapter-date-fns';
 import { Line } from 'react-chartjs-2';
-
-ChartJS.register(
-    CategoryScale,
-    LinearScale,
-    PointElement,
-    LineElement,
-    TimeScale,
-    Title,
-    Tooltip,
-    Legend
-);
+import 'chart.js/auto';
 
 const MemoryMonitoringPage = () => {
-    const [memoryData, setMemoryData] = useState([]);
-    const location = useLocation();
+    const [chartData, setChartData] = useState({
+        labels: [],
+        datasets: [
+            {
+                label: 'Memory Usage',
+                data: [],
+                borderColor: 'rgba(75, 192, 192, 1)',
+                borderWidth: 1,
+            },
+        ],
+    });
 
     useEffect(() => {
-        const queryParams = new URLSearchParams(location.search);
-        const osType = queryParams.get('osType');
-
-        const eventSource = new EventSource(`http://localhost:8080/monitoring/memory?osType=${osType}`);
-
+        const eventSource = new EventSource('http://localhost:8080/monitoring/memory');
         eventSource.onmessage = (event) => {
-            const newMemoryPoint = JSON.parse(event.data);
-            const newEntry = {
-                timestamp: new Date().toISOString(),
-                memoryUsage: newMemoryPoint.memoryUsage
-            };
-            setMemoryData((prevData) => [...prevData, newEntry]);
+            const point = JSON.parse(event.data);
+
+            console.log('Incoming point:', point); // Log each incoming point
+
+            // Append to chart data
+            setChartData(prevData => ({
+                labels: [...prevData.labels, new Date(point.timestamp).toLocaleTimeString()],
+                datasets: [{
+                    ...prevData.datasets[0],
+                    data: [...prevData.datasets[0].data, point.memoryUsage]
+                }]
+            }));
         };
 
         eventSource.onerror = (error) => {
@@ -53,52 +40,12 @@ const MemoryMonitoringPage = () => {
         return () => {
             eventSource.close();
         };
-    }, [location]);
-
-    const data = {
-        labels: memoryData.map((point) => point.timestamp),
-        datasets: [
-            {
-                label: 'Memory Usage (%)',
-                data: memoryData.map((point) => point.memoryUsage),
-                fill: false,
-                borderColor: 'rgb(255, 99, 132)',
-                backgroundColor: 'rgba(255, 99, 132, 0.5)',
-            },
-        ],
-    };
-
-    const options = {
-        scales: {
-            x: {
-                type: 'time',
-                time: {
-                    unit: 'second',
-                    displayFormats: {
-                        second: 'h:mm:ss a'
-                    }
-                },
-            },
-            y: {
-                beginAtZero: true,
-            },
-        },
-        responsive: true,
-        plugins: {
-            legend: {
-                position: 'top',
-            },
-            title: {
-                display: true,
-                text: 'Memory Usage Over Time',
-            },
-        },
-    };
+    }, []); // Empty dependency array ensures this effect runs once on mount
 
     return (
         <div>
-            <h1>Memory Monitoring</h1>
-            <Line data={data} options={options} />
+            <h2>Memory Usage Graph</h2>
+            <Line data={chartData} />
         </div>
     );
 };
